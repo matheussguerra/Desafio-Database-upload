0cef96485a228e051eacc74af62a1f13b49bddfa
Matheus Guerra <guerramatheus2@gmail.com>
2020-09-08 01:53:48 -0300

    base criada

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,32 @@
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, JoinColumn, ManyToOne, Column } from 'typeorm';
+
+import Category from './Category'
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({
+    name: 'category_id'
+  })
   category_id: string;
 
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,20 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ category, title, type, value }: Request): Promise<Transaction> {
+
+
+
+    return transaction;
   }
 }
 
